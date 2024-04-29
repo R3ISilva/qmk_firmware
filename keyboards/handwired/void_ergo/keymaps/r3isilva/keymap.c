104e7fcc4e47bff7b2c22cb36cc07cf30607754f
João Reis <joao.reis@janeladigital.com>
2024-04-29 12:17:57 +0100

    cleaned file

--- keyboards/handwired/void_ergo/keymaps/r3isilva/keymap.c
+++ keyboards/handwired/void_ergo/keymaps/r3isilva/keymap.c
@@ -3,7 +3,6 @@
 // Defines names for use in layer keycodes and the keymap
 enum layer_names {
     _BASE,
-    _RAISE,
     _NAV,
     _NUMBERS,
     _ADJUST
@@ -12,18 +11,10 @@ enum layer_names {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_BASE] = LAYOUT_split_3x6_5(
-        KC_TRNS,    KC_Q,                KC_W,                  KC_E,                    KC_R,                   KC_T,           KC_Y,       KC_U,                    KC_I,                KC_O,                MT(KC_PRINT_SCREEN, KC_P),  KC_TRNS,
-        KC_TRNS,    MT(MOD_LGUI, KC_A),  MT(MOD_LALT, KC_S),    MT(MOD_LCTL, KC_D),      MT(MOD_LSFT, KC_F),     KC_G,           KC_H,       MT(MOD_LSFT, KC_J),      MT(MOD_LCTL, KC_K),  MT(MOD_LALT, KC_L),  MT(MOD_LGUI, UC(0x00E7)),      KC_TRNS,
-        KC_TRNS,    KC_Z,                KC_X,                  KC_C,                    KC_V,                   KC_B,           KC_N,       KC_M,                    KC_COMM,             KC_DOT,              KC_TRNS,                    KC_TRNS,
-                    KC_CAPS,             MT(KC_TRNS, KC_ESC),   LT(_NAV, KC_SPC),      LT(_ADJUST, KC_ENT),    KC_F18,         KC_F19,      MT(KC_TRNS, KC_TAB),      LT(_NUMBERS, KC_BSPC), KC_DEL,              KC_RGUI
-    ),
-
-    [_NUMBERS] = LAYOUT_split_3x6_5(
-        KC_TRNS,    KC_TRNS,  KC_TRNS,  KC_TRNS,    KC_TRNS,   KC_TRNS,                  KC_LBRC,     KC_1,        KC_2,       KC_3,     KC_RBRC,       KC_TRNS,
-        KC_TRNS,    KC_RGUI,  KC_LALT,  KC_LCTL,    KC_LSFT,   KC_TRNS,                  KC_SEMICOLON,KC_4,        KC_5,       KC_6,     KC_EQL,        KC_TRNS,
-        KC_TRNS,    KC_TRNS,  KC_TRNS,  KC_TRNS,    KC_TRNS,   KC_TRNS,                  KC_GRAVE,    KC_7,        KC_8,       KC_9,     KC_BACKSLASH,  KC_TRNS,
-
-                    KC_TRNS,  KC_DOT,   KC_0,       KC_MINS,   KC_TRNS,                  KC_TRNS,     KC_TRNS,     KC_TRNS,    KC_TRNS,  KC_TRNS
+        KC_TRNS,    KC_Q,                KC_W,                  KC_E,                    KC_R,                   KC_T,                              KC_Y,       KC_U,                    KC_I,                  KC_O,                KC_P,                         KC_TRNS,
+        KC_TRNS,    MT(MOD_LGUI, KC_A),  MT(MOD_LALT, KC_S),    MT(MOD_LCTL, KC_D),      MT(MOD_LSFT, KC_F),     KC_G,                              KC_H,       MT(MOD_LSFT, KC_J),      MT(MOD_LCTL, KC_K),    MT(MOD_LALT, KC_L),  MT(MOD_LGUI, UC(0x00E7)),     KC_TRNS,
+        KC_TRNS,    KC_Z,                KC_X,                  KC_C,                    KC_V,                   KC_B,                              KC_N,       KC_M,                    KC_COMM,               KC_DOT,              KC_TRNS,                      KC_TRNS,
+                    KC_CAPS,             MT(KC_TRNS, KC_ESC),   LT(_NAV, KC_SPC),        LT(_ADJUST, KC_ENT),    KC_F18,                            KC_F19,     MT(KC_TRNS, KC_TAB),     LT(_NUMBERS, KC_BSPC), KC_DEL,              KC_RGUI
     ),
 
     [_NAV] = LAYOUT_split_3x6_5(
@@ -33,14 +24,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                     KC_TRNS,  KC_TRNS,  KC_TRNS,    KC_TRNS,   KC_TRNS,                  KC_TRNS,    KC_TRNS,        KC_TRNS,       KC_TRNS,     KC_TRNS
     ),
 
-    [_RAISE] = LAYOUT_split_3x6_5(
-        KC_TRNS,    KC_TRNS,  KC_TRNS,  KC_COLON,   KC_AT,     KC_HASH,                   KC_EQL,     KC_SLSH,   KC_COLON,   KC_EXLM,  KC_TRNS,  KC_TRNS,
-        KC_TRNS,    KC_TRNS,  KC_QUOT,  KC_LPRN,    KC_RPRN,   KC_ASTR,                  KC_SCLN,    KC_LCBR,   KC_RCBR,    KC_QUES,  KC_TRNS,  KC_TRNS,
-        KC_TRNS,    KC_TRNS,  KC_TRNS,  KC_LBRC,    KC_RBRC,   KC_TRNS,                  KC_MINS,    KC_PLUS,   KC_COMM,    KC_DOT,   KC_TRNS,  KC_TRNS,
+    [_NUMBERS] = LAYOUT_split_3x6_5(
+        KC_TRNS,    KC_TRNS,  KC_TRNS,  KC_TRNS,    KC_TRNS,   KC_TRNS,                  KC_LBRC,     KC_1,        KC_2,       KC_3,     KC_RBRC,       KC_TRNS,
+        KC_TRNS,    KC_RGUI,  KC_LALT,  KC_LCTL,    KC_LSFT,   KC_TRNS,                  KC_SEMICOLON,KC_4,        KC_5,       KC_6,     KC_EQL,        KC_TRNS,
+        KC_TRNS,    KC_TRNS,  KC_TRNS,  KC_TRNS,    KC_TRNS,   KC_TRNS,                  KC_GRAVE,    KC_7,        KC_8,       KC_9,     KC_BACKSLASH,  KC_TRNS,
 
-                    KC_TRNS,  KC_TRNS,  KC_TRNS,    KC_TRNS,   KC_TRNS,                  KC_TRNS,    KC_TRNS,   KC_TRNS,    KC_TRNS,  KC_TRNS
+                    KC_TRNS,  KC_DOT,   KC_0,       KC_MINS,   KC_TRNS,                  KC_TRNS,     KC_TRNS,     KC_TRNS,    KC_TRNS,  KC_TRNS
     ),
-
+    
     [_ADJUST] = LAYOUT_split_3x6_5(
         KC_TRNS,    KC_TRNS,  KC_PGUP,  KC_UP,      KC_PGDN,  KC_TRNS,                  KC_TRNS,      KC_F1,    KC_F2,      KC_F3,    KC_F10,  KC_TRNS,
         KC_TRNS,    KC_TRNS,  KC_LEFT,  KC_DOWN,    KC_RIGHT, KC_TRNS,                  KC_TRNS,      KC_F4,    KC_F5,      KC_F6,    KC_F11,  KC_TRNS,

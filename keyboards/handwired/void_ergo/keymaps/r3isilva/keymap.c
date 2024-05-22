9b8f9abac154549fb03f25649f8949638ef37687
João Reis <joao.reis@janeladigital.com>
2024-05-22 17:24:42 +0100

    Added Media Controls

--- keyboards/handwired/void_ergo/keymaps/r3isilva/keymap.c
+++ keyboards/handwired/void_ergo/keymaps/r3isilva/keymap.c
@@ -5,16 +5,17 @@ enum layer_names {
     _BASE,
     _NAV,
     _NUMBERS,
-    _ADJUST
+    _FUN,
+    _MEDIA
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_BASE] = LAYOUT_split_3x6_5(
         KC_TRNS,    KC_Q,                KC_W,                  KC_E,                    KC_R,                   KC_T,                              KC_Y,       KC_U,                    KC_I,                  KC_O,                KC_P,                         KC_TRNS,
-        KC_TRNS,    MT(MOD_LGUI, KC_A),  MT(MOD_LALT, KC_S),    MT(MOD_LCTL, KC_D),      MT(MOD_LSFT, KC_F),     KC_G,                              KC_H,       MT(MOD_LSFT, KC_J),      MT(MOD_LCTL, KC_K),    MT(MOD_LALT, KC_L),  MT(MOD_LGUI, UC(0x00E7)),     KC_TRNS,
+        KC_TRNS,    MT(MOD_LGUI, KC_A),  MT(MOD_LALT, KC_S),    MT(MOD_LCTL, KC_D),      MT(MOD_LSFT, KC_F),     KC_G,                              KC_H,       MT(MOD_LSFT, KC_J),      MT(MOD_LCTL, KC_K),    MT(MOD_LALT, KC_L),  MT(MOD_LGUI, KC_QUOTE),       KC_TRNS,
         KC_TRNS,    KC_Z,                KC_X,                  KC_C,                    KC_V,                   KC_B,                              KC_N,       KC_M,                    KC_COMM,               KC_DOT,              KC_SLASH,                     KC_TRNS,
-                    KC_CAPS,             MT(KC_TRNS, KC_ESC),   LT(_NAV, KC_SPC),        LT(_ADJUST, KC_ENT),    KC_F18,                            KC_F19,     MT(KC_TRNS, KC_TAB),     LT(_NUMBERS, KC_BSPC), KC_DEL,              KC_RGUI
+                    KC_CAPS,             LT(_MEDIA, KC_ESC),    LT(_NAV, KC_SPC),        LT(KC_TRNS, KC_ENT),    KC_F18,                            KC_F19,     LT(_FUN, KC_TAB),        LT(_NUMBERS, KC_BSPC), KC_DEL,              KC_RGUI
     ),
 
     [_NAV] = LAYOUT_split_3x6_5(
@@ -32,18 +33,26 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                     KC_TRNS,  KC_DOT,   KC_0,       KC_MINS,   KC_TRNS,                  KC_TRNS,     KC_TRNS,     KC_TRNS,    KC_TRNS,  KC_TRNS
     ),
     
-    [_ADJUST] = LAYOUT_split_3x6_5(
+    [_FUN] = LAYOUT_split_3x6_5(
         KC_TRNS,    KC_TRNS,  KC_PGUP,  KC_UP,      KC_PGDN,  KC_TRNS,                  KC_TRNS,      KC_F1,    KC_F2,      KC_F3,    KC_F10,  KC_TRNS,
         KC_TRNS,    KC_RGUI,  KC_LALT,  KC_LCTL,    KC_LSFT,  KC_TRNS,                  KC_TRNS,      KC_F4,    KC_F5,      KC_F6,    KC_F11,  KC_TRNS,
         KC_TRNS,    KC_TRNS,  KC_HOME,  KC_TRNS,    KC_END,   KC_TRNS,                  KC_TRNS,      KC_F7,    KC_F8,      KC_F9,    KC_F12,  KC_TRNS,
 
                     KC_TRNS,  KC_TRNS,  KC_TRNS,    KC_TRNS,  KC_TRNS,                  KC_TRNS,      KC_TRNS,  KC_TRNS,    KC_TRNS,  KC_TRNS
-    )
+    ),
+    
+    [_MEDIA] = LAYOUT_split_3x6_5(
+        KC_TRNS,    KC_TRNS,  KC_TRNS,  KC_TRNS,    KC_TRNS,   KC_TRNS,                  KC_TRNS,    KC_TRNS,              KC_TRNS,                KC_TRNS,           KC_TRNS,                KC_TRNS,
+        KC_TRNS,    KC_RGUI,  KC_LALT,  KC_LCTL,    KC_LSFT,   KC_TRNS,                  KC_TRNS,    KC_MEDIA_PREV_TRACK,  KC_AUDIO_VOL_DOWN,      KC_AUDIO_VOL_UP,   KC_MEDIA_NEXT_TRACK,    KC_TRNS,
+        KC_TRNS,    KC_TRNS,  KC_TRNS,  KC_TRNS,    KC_TRNS,   KC_TRNS,                  KC_TRNS,    KC_TRNS,              KC_TRNS,                KC_TRNS,           KC_TRNS,                KC_TRNS,
+
+                    KC_TRNS,  KC_TRNS,  KC_TRNS,    KC_TRNS,   KC_TRNS,                  KC_TRNS,    KC_MEDIA_STOP,        KC_MEDIA_PLAY_PAUSE,    KC_AUDIO_MUTE,     KC_TRNS
+    ),
 };
 
 layer_state_t layer_state_set_user(layer_state_t state)
 {
-    /*state = update_tri_layer_state(state, _NAV, _RAISE, _ADJUST);
+    /*state = update_tri_layer_state(state, _NAV, _RAISE, _FUN);
 
     int16_t layer = _BASE;
 
@@ -55,9 +64,9 @@ layer_state_t layer_state_set_user(layer_state_t state)
     {
         layer = _RAISE;
     }
-    else if (IS_LAYER_ON_STATE(state, _ADJUST))
+    else if (IS_LAYER_ON_STATE(state, _FUN))
     {
-        layer = _ADJUST;
+        layer = _FUN;
     }
 
     tap_code16(KC_F13 + layer); send input that layer has been changed with F13 + layer */
